719c7177b10fcfcac239632c538234a02199d07c
Owais-cmd <owaistab18@gmail.com>
2026-05-17 15:53:40 +0530

    Fix typo in import statement for aida-sdk

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import aida fom 'aida-sdk'; // Use the new package name you published!
+import aida fm 'aida-sdk'; // Use the new package name you published!
 
 // 1. Initialize the SDK
 aida.init({
