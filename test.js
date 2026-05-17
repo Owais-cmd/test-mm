6bddfd5a0d4edeb965514b632eb24c584ed4fb79
Owais-cmd <owaistab18@gmail.com>
2026-05-17 15:45:04 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import aida om 'aida-sdk'; // Use the new package name you published!
+import aida fom 'aida-sdk'; // Use the new package name you published!
 
 // 1. Initialize the SDK
 aida.init({
