bc11eecbfce1e7723f28756c1d62518c15634249
Owais-cmd <owaistab18@gmail.com>
2026-05-22 21:37:35 +0530

    Update test.js

--- test.js
+++ test.js
@@ -12,6 +12,7 @@ aida.init({
 // 2. Simulate a fatal application error
 console.log("Starting application..")
 console.warn("Memory usage is high..")
+consol.log("CPU Uage is increasing")
 
 // Throwing this unhandled error will be automatically intercepted by the SDK!
 throw new Error("CRITICAL: Database connection lost during transaction!");
