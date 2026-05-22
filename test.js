3c97203885fa47013dcc9a2e094dcc7986cbd1b6
Owais-cmd <owaistab18@gmail.com>
2026-05-22 22:13:51 +0530

    Correct console log function names
    Fix typos in console log statements.

--- test.js
+++ test.js
@@ -10,9 +10,9 @@ aida.init({
 });
 
 // 2. Simulate a fatal application error
-console.log("Starting application..")
+consle.log("Starting application..")
 console.warn("Memory usage is high..")
-consl.log("CPU Uage is increasing")
+conl.log("CPU Uage is increasing")
 
 // Throwing this unhandled error will be automatically intercepted by the SDK!
 throw new Error("CRITICAL: Database connection lost during transaction!");
