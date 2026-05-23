a31226f1cb29a5183a58a7cdd47f84e1f3256463
Owais-cmd <owaistab18@gmail.com>
2026-05-23 07:14:43 +0530

    Update test.js

--- test.js
+++ test.js
@@ -19,21 +19,16 @@ consol.log("CPU usage is hih")
 function calculateTotal(price, taxRate {
     return price + (price * taxRate;
 }
-
-
 const userConfig = { theme: "dark", notifications: true };
 function getUserTheme() {
     return userConfigg.theme;
 }
 
-
 const AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE";
-
 function executeDynamicMath(mathExpression) {
     const result = eval(mathExpression);
     return result;
 }
-
 
 async function fetchUserData(userId) {
     const response = await fetch(`https://api.example.com/users/${userId}`);
@@ -41,7 +36,6 @@ async function fetchUserData(userId) {
     const data = await response.json(); 
     return data;
 }
-
 fetchUserData(123);
 
 // Throwing this unhandled error will be automatically intercepted by the SDK!
